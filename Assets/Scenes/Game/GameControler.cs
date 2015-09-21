97cb57c9b0e5f4ef87f8c5b288e82ef7ab8f1fde
Thomas Vardy <tomvardy.home@googlemail.com>
2015-09-21 22:50:08 +0100

    added colliders for room and a ball

---
+++ Assets/Scenes/Game/GameControler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is the game controller, it handles all the game logic.
+/// </summary>
+public class GameControler : MonoBehaviour {
+
+    /// <summary>
+    /// The floor game object.
+    /// </summary>
+    public GameObject Floor;
+
+    /// <summary>
+    /// This is the ball game object.
+    /// </summary>
+    public GameObject Ball;
+
+	/// <summary>
+    /// Starts the script.
+    /// </summary>
+	void Start () {
+	    
+
+
+	}
+	
+    /// <summary>
+    /// This updates the script,and is called once per frame.
+    /// </summary>
+	void Update () {
+	
+	}
+    public void OnBallCollide(GameObject obj)
+    {
+        if (obj == Ball)
+        {
+            // This is end of game
+        }
+
+    }
+
+    
+}
